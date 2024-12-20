544da25c53fc990e3e0699fefaf9963a1928328f
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-12-20 22:28:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from os import getenv
 
 # Ensure FSUB_1 and FSUB_2 are defined
 #Make sure you give channel id of those channel in which you want to create join request in RFSUB
-FSUB_1 = int(getenv('FSUB_1', '-1002374330304')) #This chat will not create join request
-FSUB_2 = int(getenv('FSUB_2', '-1002374330304')) #This chat will not create join request
-RFSUB  = [int(i) for i in getenv("RFSUB", "0").split()] #Give space spearated value of the channel which you want that user will create a join request for leave it 0 if you don't want any request type force sub
+FSUB_1 = int(getenv('FSUB_1', '-1002338546342')) #This chat will not create join request
+FSUB_2 = int(getenv('FSUB_2', '-1002250556684')) #This chat will not create join request
+RFSUB  = [int(i) for i in getenv("RFSUB", "-1002256459104").split()] #Give space spearated value of the channel which you want that user will create a join request for leave it 0 if you don't want any request type force sub
 
 # Now define FSUB using FSUB_1 and FSUB_2
 FSUB = [FSUB_1, FSUB_2]
@@ -18,10 +18,10 @@ API_ID2 = int(getenv('API_ID_BOT1', '3510496'))  # Bot2 API ID
 API_HASH2 = getenv('API_HASH_BOT1', 'c65647776bb4e93defc9504571d2b990')  # Bot2 API Hash
 
 #Bot token for the bot
-BOT_TOKEN = getenv('BOT_TOKEN', '7041654616:AAHCsdChgpned-dlBEjv-OcOxSi_mY5HRjI')
-BOT_TOKEN_2 = getenv('BOT_TOKEN_2', '7643758086:AAFZkPY5yHGxeF4F7oQnxIFftSK05B7u6jY')
+BOT_TOKEN = getenv('BOT_TOKEN', '7099022623:AAHF5XCTdVgREoJWvK6sRJedYIso35E0XpE')
+BOT_TOKEN_2 = getenv('BOT_TOKEN_2', '7867772211:AAEv6qY38CN2t9_8S-JGXnqsLFrPZkNv2y4')
 
-BUY_LINK = getenv("Buy Link") #Link to the acc or channel on which user will be redirected when he clicks on Buy now in voice msg
+BUY_LINK = getenv("@cutegirlTG") #Link to the acc or channel on which user will be redirected when he clicks on Buy now in voice msg
 OWNER_ID = int(getenv("OWNER_ID"))
 SUDO_USERS = [int(x) for x in getenv('SUDO_USERS', '6604279354 6104594076').split()]
 SUDO_USERS.append(OWNER_ID)
@@ -31,12 +31,12 @@ MONGO_DB_URI = getenv('MONGO_DB_URI', 'mongodb+srv://manik:manik11@cluster0.iam3
 DB_CHANNEL_ID = int(getenv('DB_CHANNEL_ID', '-1002374330304'))
 DB_CHANNEL_2_ID = int(getenv('DB_CHANNEL_2_ID', '-1002374330304'))
 LOG_CHANNEL_ID = int(getenv('LOG_CHANNEL_ID', '-1002374330304'))
-LOG_CHANNEL_ID2 = int(getenv('LOG_CHANNEL_ID2', '0')) #Keep it 0 if you don't have second log channel
-FEEDBACK_CHANNEL = int(getenv("FEEDBACK_CHANNEL", 0)) # Keep it 0 if you don't want to give feed back channel id
-AFTER_FEEDBACK = int(getenv("AFTER_FEEDBACK", 0)) #Channel on which post will be forwarded when the owner clicks on approve
-AUTO_SAVE_CHANNEL = int(getenv("AUTO_SAVE_CHANNEL", 0)) #Channel where all the content from fsub1 will be saved
+LOG_CHANNEL_ID2 = int(getenv('LOG_CHANNEL_ID2', '-1002385675587')) #Keep it 0 if you don't have second log channel
+FEEDBACK_CHANNEL = int(getenv("FEEDBACK_CHANNEL", -1002330344157)) # Keep it 0 if you don't want to give feed back channel id
+AFTER_FEEDBACK = int(getenv("AFTER_FEEDBACK", -1002385675587)) #Channel on which post will be forwarded when the owner clicks on approve
+AUTO_SAVE_CHANNEL = int(getenv("AUTO_SAVE_CHANNEL", -1002385675587)) #Channel where all the content from fsub1 will be saved
 
-AUTO_DELETE_TIME = int(getenv('AUTO_DELETE_TIME', '0'))  # Enter time in seconds, keep it 0 for disabling.
+AUTO_DELETE_TIME = int(getenv('AUTO_DELETE_TIME', '60'))  # Enter time in seconds, keep it 0 for disabling.
 
 MUST_VISIT_LINK = "https://t.me/Ultra_XYZ/14"
 
@@ -61,7 +61,7 @@ CONTENT_SAVER = True
 
 EXPIRY_TIME = 30  # In days
 
-AUTO_SAVE_CHANNEL_ID = -1002374330304
+AUTO_SAVE_CHANNEL_ID = -1002385675587
 
 PHONE_NUMBER_IMAGE = "https://graph.org/file/2821554b6b082eb8741dc.jpg"
 
